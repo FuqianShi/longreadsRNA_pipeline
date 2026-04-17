277f35fe546295244d4ab23b3c1f974d7253159b
Fuqian Shi <58180663+FuqianShi@users.noreply.github.com>
2026-04-17 11:06:30 -0400

    Update and rename files_path_structure.txt to files_path_structure.sh

---
+++ files_path_structure.sh
@@ -0,0 +1,3 @@
+mkdir -p pacbio_lrna/{hifi_reads,refs,results,logs,scripts,containers}
+cd pacbio_lrna
+mkdir -p results/{00_qc,01_fastq,02_align,03_isoseq,04_sqanti3,05_flair,06_fusion,07_splicing,08_tegene,09_reports}
